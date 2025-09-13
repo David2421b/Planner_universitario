298ad96686e7118d94b5276bf2d67310ba03deaa
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-09-12 23:59:42 -0500

    correccion de errores en la logica basica

--- src/model/user_model.py
+++ src/model/user_model.py
@@ -6,11 +6,12 @@ from config.firebase_config import db, auth
 
 class User:
     @staticmethod
-    def create_user(email, password, name, ):
+    def create_user(email, password, name):
         user = auth.create_user_with_email_and_password(email, password)
         user_id = user["localId"]
         user_data = {"user_id": user_id,
                      "name": name}
+        db.child("users").child(user_id).set(user_data)
         return user
 
     @staticmethod
@@ -21,6 +22,6 @@ class User:
     
 valor = int(input("Ingrese 1 o 2: "))
 if valor == 1:
-    User.create_user("davidh1@gmail.com", "123456789")
+    User.create_user("davidh1@gmail.com", "123456789", "David")
 else:
     print(User.get_users())
