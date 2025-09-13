93a79ec71fded901dc2d2009a414bbb898de7ad5
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-09-12 23:51:52 -0500

    primeras pruebas de usar DB en firebase

--- src/model/user_model.py
+++ src/model/user_model.py
@@ -6,13 +6,21 @@ from config.firebase_config import db, auth
 
 class User:
     @staticmethod
-    def create_user(email, password):
+    def create_user(email, password, name, ):
         user = auth.create_user_with_email_and_password(email, password)
+        user_id = user["localId"]
+        user_data = {"user_id": user_id,
+                     "name": name}
         return user
-    
+
     @staticmethod
     def get_users():
         return db.child("users").get().val()
     
 
-User.create_user("david@gmail.com", "123456789")
+    
+valor = int(input("Ingrese 1 o 2: "))
+if valor == 1:
+    User.create_user("davidh1@gmail.com", "123456789")
+else:
+    print(User.get_users())
