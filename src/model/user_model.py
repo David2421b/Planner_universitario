1ec7e6791577b3cf46a8b1aaca547cd6ebd52e5b
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-09-13 00:02:15 -0500

    se implementa el metodo de login con el metodo de la libreria

--- src/model/user_model.py
+++ src/model/user_model.py
@@ -7,11 +7,15 @@ from config.firebase_config import db, auth
 class User:
     @staticmethod
     def create_user(email, password, name):
-        user = auth.create_user_with_email_and_password(email, password)
+        user = auth.create_user_with_email_and_password(email, password) #Se usa el meto para crear un nuevo usuario en unthentication con email and password
         user_id = user["localId"]
         user_data = {"user_id": user_id,
                      "name": name}
         db.child("users").child(user_id).set(user_data)
+        return user
+
+    def login(email, password):
+        user = auth.sign_in_with_email_and_password(email, password) #Se usa el metodo para consutlar en authentication por email and password
         return user
 
     @staticmethod
