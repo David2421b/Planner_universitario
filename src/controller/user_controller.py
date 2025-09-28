9e5b8de47bc4f415061d14c384cf3e1456e13ea8
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-09-27 21:48:04 -0500

    statickmethod

--- src/controller/user_controller.py
+++ src/controller/user_controller.py
@@ -13,6 +13,7 @@ class UserController:
         db.child("users").child(user_id).set(user_data)
         return user
 
+    @staticmethod
     def login(email, password):
         user = auth.sign_in_with_email_and_password(email, password) #Se usa el metodo para consutlar en authentication por email and password
         return user
