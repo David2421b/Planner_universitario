bd309258589e808f2b3d4160e0b59b2d39a2fd08
camilo renteria <yhoider200093@gmail.com>
2025-09-29 20:55:10 -0500

    Vistas

--- backend/src/model/user_model.py
+++ backend/src/model/user_model.py
@@ -1,18 +1,29 @@
+import firebase_admin
+
 import sys
 sys.path.append(".")
 
-from config.firebase_config import db, auth
+from config.firebase_config import db, auth, admin_auth
 
 class UserController:
     @staticmethod
-    def create_user(email, password, name):
+    def create_user(email, password, name, role):
         user = auth.create_user_with_email_and_password(email, password) #Se usa el meto para crear un nuevo usuario en unthentication con email and password
         user_id = user["localId"]
+
+        try:
+            admin_auth.set_custom_user_claims(user_id, {"rol": role})
+
+        except Exception as e:
+            print(f"Error al asignar el custom claim: {e}")
+
+
         user_data = {"user_id": user_id,
-                     "name": name}
+                     "name": name,
+                     "user_rol": role}
         db.child("users").child(user_id).set(user_data)
         return user
-
+                    
     @staticmethod
     def login(email, password):
         user = auth.sign_in_with_email_and_password(email, password) #Se usa el metodo para consutlar en authentication por email and password
@@ -21,11 +32,3 @@ class UserController:
     @staticmethod
     def get_users():
         return db.child("users").get().val()
-    
-
-    
-valor = int(input("Ingrese 1 o 2: "))
-if valor == 1:
-    UserController.create_user("davidh1@gmail.com", "123456789", "David")
-else:
-    print(UserController.get_users())
